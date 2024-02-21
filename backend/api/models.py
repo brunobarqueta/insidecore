3edf032c3fa5cb5a3864df9b98b4d6155b04ad1d
Mateus Laurentino de Andrade <mateus_laurentino@outlook.com>
2024-02-21 00:12:28 -0300

    Ajustar usuario customizado e validações de entrada

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fullName = models.CharField(max_length=255)
-    cpf = models.CharField(max_length=14)
-    phone = models.CharField(max_length=20)
-
+class CustomUser(AbstractUser):       
+    username = models.CharField(("username"),max_length=150, blank=True)
+    email = models.EmailField(("email address"), unique=True,
+                help_text=("Obrigatório. 50 caracteres ou menos. Somente letras, dígitos e @/./+/-/_."))
+    fullname = models.CharField(max_length=255, help_text=("Obrigatório."))
+    cpf = models.CharField(max_length=14, help_text=('Obrigatório. Informe o cpf no seguinte formato: XXX.XXX.XXX-XX'))
+    phone = models.CharField(max_length=20, help_text=("Obrigatório."))
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    8
     def __str__(self):
-        self.user.username
+        return self.fullname
--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import User
+from .models import CustomUser
+from . import validators
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+import re
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -20,27 +21,54 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(error_messages={'invalid': 'Por favor, insira um endereço de e-mail válido.',})
 
     class Meta:
-        model = User
-        fields = ('username', 'password', 'password2')
+        model = CustomUser
+        fields = ('email' ,'fullname', 'cpf', 'phone', 'password', 'password2')
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        if not validators.validate_fullname(attrs['fullname']):
+            raise serializers.ValidationError({"fullname": "Informe nome e sobrenome."})
+        
+        if not validators.validate_cpf_mask(attrs['cpf']):
+            raise serializers.ValidationError({"cpf": "Cpf está no formato inválido."})
+        
+        if not validators.validate_phone(attrs['phone']):
+            raise serializers.ValidationError({"phone": "Numero de telefone é inválido."})
+            
+        if not validators.validate_cpf(attrs['cpf']):
+            raise serializers.ValidationError({"cpf": "Cpf informado é inválido"})
+            
+        if not validators.validate_passwords(attrs['password'], attrs['password2']):
             raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
-
+                {"password2": "Senhas informadas são diferentes."})
+        
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username']
-        )
-
+        fullname = str(validated_data['fullname']).title()
+        phone = re.sub(r'[^\d]','',validated_data['phone'])
+        
+        split_name = fullname.split()
+        first_name = split_name[0]
+        last_name = split_name[-1]
+        
+        try:
+            user = CustomUser.objects.create(
+                email=validated_data['email'],
+                cpf=validated_data['cpf'],
+                fullname=fullname,
+                phone=phone,
+                first_name=first_name,
+                last_name=last_name
+            )
+        except:
+            raise serializers.ValidationError({"Erro": "Erro ao cadastrar usuario."})
+        
         user.set_password(validated_data['password'])
         user.save()
-
+        
         return user
